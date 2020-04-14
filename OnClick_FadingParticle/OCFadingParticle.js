d495cd1d65440f3c3abad5e30cf42db54c67ed53
carloserlemus <carlosriveralemus@gmail.com>
2020-04-13 19:26:25 -0700

    Experimenting with connecting onclick to particle effect.

--- OnClick_FadingParticle/OCFadingParticle.js
+++ OnClick_FadingParticle/OCFadingParticle.js
@@ -1,12 +1,32 @@
-let ctx = document.getElementById('ctx').getContext('2d');
+let ctx = document.getElementById('ctx').getContext('2d');  
+let i = 0;
+
 
 let drawBox = (x, y, height, width, r, g, b, t) => {
     ctx.fillStyle = `rgba(${r},${g},${b},${t})`
     ctx.fillRect(x, y, height, width)
 }
 
-document.onclick = () => {
-    for (i = 0; i < 200; i += 10){
-        drawBox(100+i, 100+i, 5, 5, 100, 125, 255, 1)
-    }
+let drawBoxes = () => {
+    ctx.clearRect(0, 0, 500, 500)
+    drawBox(100+i, 100+i, 5, 5, 100, 125, 255, 1)
+    i += 10;
 }
+
+let counter = 0;
+
+let animatePixel = () => {
+    let interval = setInterval(function(){
+        counter++
+        if(counter === 5){
+            clearInterval(interval);
+        }
+        drawBoxes()
+    }, 1000/45);
+}
+
+
+document.onclick = () => {
+    animatePixel()
+}
+
